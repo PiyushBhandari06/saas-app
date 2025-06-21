0ee3d3064f6aada25dc8c65444a1f244458152ae
PiyushBhandari06 <bhandaripiyush2003@gmail.com>
2025-06-22 02:01:53 +0530

    implemented basic ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,49 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import Companioncard from '@/components/Companioncard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
+import { recentSessions } from "@/constants/index"
 
 const Page = () => {
   return (
-    <div>
-    <h1 className='text-2xl underline'>Welcome to my SaaS App</h1>
+    <main>
+    <h1 className='text-2xl underline'>Popular Companions</h1>
 
-    <Button>Let's get started</Button>
-    </div>
+    <section className='home-section'>
+      
+      <Companioncard
+        id="123"
+        name="Neura the Brainy Explorer"
+        topic="Neural Network of the Brain"
+        subject="Science"
+        duration={45}
+        color="#e5d0ff"
+      />
+      <Companioncard
+        id="456"
+        name="Countsy the Number Wizard"
+        topic="Derivatives & Integrals"
+        subject="Maths"
+        duration={30}
+        color="#ffda6e"
+      />
+      <Companioncard
+        id="789"
+        name="Verba the Vocabulary Builder"
+        topic="English Literature "
+        subject="Language"
+        duration={30}
+        color="#BDE7FF"
+      />
+    </section>
+    <section className='home-section'>
+      <CompanionsList
+        title="Recently Completed Sessions"
+        companions={recentSessions}
+        classNames="w-2/3  max-lg:w-full"
+      />
+      <CTA/>
+    </section>
+    </main>
   )
 }
 
